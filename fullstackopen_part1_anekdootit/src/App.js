9282025f584c3df9fe2c5d7469c0751c24ce2d52
makevs <make@texas.fail>
2023-04-17 21:38:56 +0300

    new file:   fullstackopen_part1_anekdootit/.gitignore 	new file:   fullstackopen_part1_anekdootit/README.md 	new file:   fullstackopen_part1_anekdootit/package-lock.json 	new file:   fullstackopen_part1_anekdootit/package.json 	new file:   fullstackopen_part1_anekdootit/public/favicon.ico 	new file:   fullstackopen_part1_anekdootit/public/index.html 	new file:   fullstackopen_part1_anekdootit/public/logo192.png 	new file:   fullstackopen_part1_anekdootit/public/logo512.png 	new file:   fullstackopen_part1_anekdootit/public/manifest.json 	new file:   fullstackopen_part1_anekdootit/public/robots.txt 	new file:   fullstackopen_part1_anekdootit/src/App.js 	new file:   fullstackopen_part1_anekdootit/src/index.js

---
+++ fullstackopen_part1_anekdootit/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Random = () => Math.floor(Math.random() * 8)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const points = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0}
+  const copy = {...points}
+  const upVote = () => copy[selected] += 1
+
+  return (
+    <div>
+      {anecdotes[selected]}<br></br>
+      <p>has {copy[selected]} votes</p>
+      <Button handleClick={() => upVote()} text="vote" />
+      <Button handleClick={() => setSelected(Random)} text="next anecdote" />
+    </div>
+  )
+}
+
+export default App
